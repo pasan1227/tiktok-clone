50213af950cad49a07df72e7012e47d5a3f4c78c
pasan1227 <pasanratnayake@gmail.com>
2021-07-04 10:48:11 +0530

    video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import Video from './Video';
 
 function App() {
   return (
     <div className="app">
       <h1>TIKTOK clone</h1>
+
+      <div className="app__videos">
+        <Video />
+        <Video />
+      </div>
+      
+
+      {/* App container */}
+        {/* video */}
+        {/* video */}
+        {/* video */}
+        {/* video */}
+
     </div>
   );
 }
