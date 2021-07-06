af468c0b3f9e5eff949d8df2a95b0acce4937da1
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-07-06 16:53:26 +0530

    axios added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React from 'react';
+import axios from './axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Video from './Video';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get("/v2/posts");
+      setVideos(response.data);
+
+      return response;
+    }
+
+    fetchPosts();
+  }, []);
+
+  console.log(videos);
+
   return (
     <div className="app">
-      <h1>TIKTOK clone</h1>
-
       <div className="app__videos">
-        <Video 
-          url="https://v77.tiktokcdn.com/6a67eee598c80485ca97a05fb3b0bc3a/60e44a39/video/tos/alisg/tos-alisg-pve-0037c001/f7e68251d9d34c9885382054b0458a28/?a=1233&br=1236&bt=618&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202107060618500102340820731938D8B7&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2RsdDN5ZjN5NTMzODczM0ApMzhkODxkOWU3N2RoZjNkPGdeY2hnNnEyYmNgLS1kMTRzcy8yNjFfNi8xL15jMy1gXmE6Yw%3D%3D&vl=&vr="
-          channel="pasannn"
-          description="TikTok Cloneeeeee"
-          song="Bad Habits"
-          likes={123}
-          messages={231}
-          shares={342}
-        />
-        <Video 
-          url="https://v77.tiktokcdn.com/5ae7da3b848b2860b182651a4dd75095/60e44ada/video/tos/alisg/tos-alisg-pve-0037/9d55563b0f6f444f87baa5cf97b3bbee/?a=1180&br=4596&bt=2298&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=3&ds=3&er=&l=202107060621300102340991580339459B&lr=tiktok&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajNtPGxxOjMzNTMzODgzM0ApNTVnNzk2ODs2N2lpNGY0OWcucmltL2pibWJgLS1kLzRzczM1YzA2L15iXzBeMy5hMjU6Yw%3D%3D&vl=&vr="
-          channel="pasannn"
-          description="TikTok Cloneeeeee"
-          song="Bad Habits"
-          likes={459}
-          messages={432}
-          shares={231}
-        />
-
+        {videos.map(({ url, channel, description, likes, song, shares, messages }) => 
+        (
+          <Video
+            url={url}
+            channel={channel}
+            song={song}
+            likes={likes}
+            messages={messages}
+            description={description}
+            shares={shares}
+          />
+        )
+        )}
       </div>
-      
-
-      {/* App container */}
-        {/* video */}
-        {/* video */}
-        {/* video */}
-        {/* video */}
-
     </div>
   );
 }
