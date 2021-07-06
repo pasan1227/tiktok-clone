2ed0dad66e80797cd79d72052d8621c30b8fa9e0
pasan1227 <pasanratnayake@gmail.com>
2021-07-06 12:34:29 +0530

    remove gitignore files

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,6 @@
 const data = [
     {
-        url: "https://v77.tiktokcdn.com/a804b5f9cf53c6ded349597645a63439/60e196b6/video/tos/alisg/tos-alisg-pve-0037c001/f0735403e18049c5aa7240cb3712f28f/?a=1233&br=3092&bt=1546&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202107040508170101150450090306D37A&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzhmOzU7dTh1NTMzODczM0ApOGlkOjs0OjxkN2k0ZGlnN2dyZGdpLi8yMmVgLS1kMTRzczBgNC5hMmBfNjAzL2NhLmE6Yw%3D%3D&vl=&vr=",
+        url: "https://v77.tiktokcdn.com/6a67eee598c80485ca97a05fb3b0bc3a/60e44a39/video/tos/alisg/tos-alisg-pve-0037c001/f7e68251d9d34c9885382054b0458a28/?a=1233&br=1236&bt=618&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202107060618500102340820731938D8B7&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2RsdDN5ZjN5NTMzODczM0ApMzhkODxkOWU3N2RoZjNkPGdeY2hnNnEyYmNgLS1kMTRzcy8yNjFfNi8xL15jMy1gXmE6Yw%3D%3D&vl=&vr=",
         channel: "pasannn",
         description: "TikTok Cloneeeeee",
         song: "Bad Habits",
@@ -9,7 +9,7 @@ const data = [
         shares: "342",
     },
     {
-        url: "https://v77.tiktokcdn.com/24bdfaed8c08232e8c137d27ba04ff83/60e1b860/video/tos/alisg/tos-alisg-pve-0037c001/768b462a7fa0428ba086fdceeae1b637/?a=1233&br=2846&bt=1423&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202107040731440101150040590D0FB7F5&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzNydDN0dWs7NjMzODczNEApNWU3NzRpMzw0N2U7NjZkaGcubl4wc2loZjFgLS1kMS1zc18zLi1jXjZhXi00MS00XzE6Yw%3D%3D&vl=&vr=",
+        url: "https://v77.tiktokcdn.com/5ae7da3b848b2860b182651a4dd75095/60e44ada/video/tos/alisg/tos-alisg-pve-0037/9d55563b0f6f444f87baa5cf97b3bbee/?a=1180&br=4596&bt=2298&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=3&ds=3&er=&l=202107060621300102340991580339459B&lr=tiktok&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajNtPGxxOjMzNTMzODgzM0ApNTVnNzk2ODs2N2lpNGY0OWcucmltL2pibWJgLS1kLzRzczM1YzA2L15iXzBeMy5hMjU6Yw%3D%3D&vl=&vr=",
         channel: "pasannn",
         description: "TikTok Cloneeeeee",
         song: "Bad Habits",
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import data from './data.js';
+import Videos from './dbModel.js';
 
 // app config
 const app = express();
 const port = 9000;
 
 // middlwares
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 // DB confi
 const connection_url = 'mongodb+srv://admin:Pasan1227@cluster0.yxp0s.mongodb.net/tiktok?retryWrites=true&w=majority';
@@ -14,13 +21,38 @@ const connection_url = 'mongodb+srv://admin:Pasan1227@cluster0.yxp0s.mongodb.net
 mongoose.connect(connection_url, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+});
 
 // API endpoints
 app.get('/',(req, res) => res.status(200).send('hello world'));
 
 app.get('/v1/posts', (req, res) => res.status(200).send(data));
+
+app.get('/v2/posts', (req, res) => {
+    Videos.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.post('/v2/posts', (req, res) => {
+    // POST request is to ADD DATA to the database
+    // It will let us ADD a video DOCUMENT to the videos collection
+    const dbVideos = req.body;
+
+    // Error handling
+    Videos.create(dbVideos, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
 
 // listen
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
